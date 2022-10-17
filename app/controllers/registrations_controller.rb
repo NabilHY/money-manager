c8fdbd71215171f4816ee074584e816ca99ea53e
neo <hayounn.etd@gmail.com>
2022-10-17 19:13:57 +0100

    Fix linters

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
-    def sign_up_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
-
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
