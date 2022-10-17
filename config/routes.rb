8729c462864bb4e0bc0d8a49d79695586b1ee98e
neo <hayounn.etd@gmail.com>
2022-10-17 18:22:18 +0100

    Setup devise logic and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   get 'static_pages/splash_page'
   get 'static_pages/dashboard'
   root 'static_pages#splash_page'
