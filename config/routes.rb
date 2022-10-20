bbbae85d7a0240730bce8dfe7ff2df1b1fd0e006
neo <hayounn.etd@gmail.com>
2022-10-20 12:22:20 +0100

    Add forms for adding new groups and expenses

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'groups/show'
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :groups, only: [:index, :new, :create ] do
-    resources :expenses, only: [:index, :new]
+    resources :expenses, only: [:index, :new, :create]
   end
   get 'static_pages/splash_page'
   get 'static_pages/dashboard'
